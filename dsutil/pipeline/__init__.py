4ffae19bd482bfe7fba9f133588ed6c647682a9f
Mike Craig <mcraig82890@gmail.com>
2022-07-23 22:57:40 -0400

    Add integration test scaffolding

--- dsutil/pipeline/__init__.py
+++ dsutil/pipeline/__init__.py
@@ -1,20 +1,22 @@
-from dsutil.pipeline.entities import (
-    DatasetPipelineConfig,
-    DatasetPipelineResults,
-)
 from dsutil.pipeline.read import PipelineReader
 from dsutil.pipeline.process import PipelineProcessor
 from dsutil.pipeline.fitter import PipelineFitter
 from dsutil.pipeline.monitor import PipelineMonitor
 from dsutil.pipeline.write import PipelineWriter
+from dsutil.pipeline.entities import (
+    DatasetPipelineConfig,
+    DatasetPipelineResults,
+)
+from dsutil.pipeline.dataset_pipeline import DatasetPipeline
 
 
 __all__ = [
-    DatasetPipelineConfig,
-    DatasetPipelineResults,
     PipelineReader,
     PipelineProcessor,
     PipelineFitter,
     PipelineMonitor,
     PipelineWriter,
+    DatasetPipelineConfig,
+    DatasetPipelineResults,
+    DatasetPipeline,
 ]
---
+++ test/pipeline/test_dataset_pipeline.py
@@ -0,0 +1,49 @@
+import unittest
+
+from dsutil.pipeline import (
+    DatasetPipelineConfig,
+    DatasetPipeline,
+    PipelineReader,
+    PipelineProcessor,
+    PipelineFitter,
+    PipelineMonitor,
+    PipelineWriter,
+    DatasetPipelineResults,
+)
+
+
+class DatasetPipelineTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config = DatasetPipelineConfig(
+            name='',
+            reader=PipelineReader(),
+            processor=PipelineProcessor(),
+            fitter=PipelineFitter(),
+            monitor=PipelineMonitor(),
+            writer=PipelineWriter(filenames=['output.csv']),
+        )
+        self.pipeline = DatasetPipeline(config=self.config)
+
+    @unittest.skip
+    def test_raw_data(self) -> None:
+        pass
+
+    @unittest.skip
+    def test_processed_data(self) -> None:
+        pass
+
+    @unittest.skip
+    def test_models(self) -> None:
+        pass
+
+    @unittest.skip
+    def test_report_artifacts(self) -> None:
+        pass
+
+    @unittest.skip
+    def test_output_filenames(self) -> None:
+        pass
+
+    @unittest.skip
+    def test_run(self) -> None:
+        pass
